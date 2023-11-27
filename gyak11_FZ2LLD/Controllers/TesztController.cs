35e9e47f94d642008bc039fda3260c681c7dd327
Dominik André Filep-György <dominik.filep-gyorgy@stud.uni-corvinus.hu>
2023-11-27 15:07:35 +0100

    hajosFeladat: adatbazis kapcsolat es kiiras letrehozasa

---
+++ gyak11_FZ2LLD/Controllers/TesztController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gyak11_FZ2LLD.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = pontosIdő
+            };
+
+            //return Ok(pontosIdő);
+        }
+
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+
+            //return Ok(szoveg.ToUpper());
+        }
+    }
+}
